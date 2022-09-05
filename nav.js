272b77e22555f512b6919fa65a38da3e522ec468
Indranil Naskar <82460475+indra0421@users.noreply.github.com>
2022-09-05 21:54:17 +0530

    ,,,

--- nav.js
+++ nav.js
@@ -6,20 +6,26 @@
 const express = require("express");
 const app = express();
 
-app.get("/",(req,res) => {
-    res.send("welcome to my home page");
+app.get("/", (req, res) => {
+    res.write("<h1>welcome to my home page</h1>");
+    res.write("<h1>welcome to my home page</h1>");
+    res.send();   //write it must to avoid loading condition -- if not used page will load
+
     // console.log(req);
 })
-app.get("/about",(req,res) => {
+app.get("/about", (req, res) => {
     res.send("welcome to my About page");
 })
-app.get("/contact",(req,res) => {
-    res.send("welcome to my Contact page");
+app.get("/contact", (req, res) => {
+    res.status(300).send("welcome to my Contact page");
 })
-app.get("/temp",(req,res) => {
-    res.status(300).send("welcome to temparature page");
+app.get("/temp", (req, res) => {
+    res.send({
+        id: 1,
+        name: "indranil"
+    });
 })
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log("listening to the port 3000");
 })
